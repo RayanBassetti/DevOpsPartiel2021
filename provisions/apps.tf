cee95cd729f2a593c56346f972f5f98c223987e5
Rayan Bassetti <rayanbassetti@MacBook-Air-de-Rayan.local>
2018-12-04 11:23:24 +0100

    Fixed heroku set up

--- provisions/apps.tf
+++ provisions/apps.tf
@@ -1,1 +1,27 @@
+# Create Heroku apps for staging and production
+resource "heroku_app" "staging" {
+  name   = "app-partiel-staging-wsf"
+  region = "eu"
 
+  buildpacks = [
+    "heroku/go",
+  ]
+}
+
+resource "heroku_app" "development" {
+  name   = "app-partiel-development-wsf"
+  region = "eu"
+
+  buildpacks = [
+    "heroku/go",
+  ]
+}
+
+resource "heroku_app" "production" {
+  name   = "app-partiel-production-wsf"
+  region = "eu"
+
+  buildpacks = [
+    "heroku/go",
+  ]
+}
