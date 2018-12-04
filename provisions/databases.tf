90c1480b9b976c19a3b5d66abfff5b0895133c18
Rayan Bassetti <rayanbassetti@MacBook-Air-de-Rayan.local>
2018-12-04 11:34:52 +0100

    added databases for heroku apps

---
+++ provisions/databases.tf
@@ -0,0 +1,15 @@
+# Create a database, and configure the app to use it
+resource "heroku_addon" "database_staging" {
+  app  = "${heroku_app.staging.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "database_development" {
+  app  = "${heroku_app.staging.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "database_prod" {
+  app  = "${heroku_app.production.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
